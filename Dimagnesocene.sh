b2b7b0f73f8a471d7ece2a6faefce6b0298d93f0
Connor Briggs <cgbriggs99@gmail.com>
2023-08-24 08:19:36 -0400

    First run of computations.

---
+++ Dimagnesocene.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#$ -q gen4.q
+#$ -N dimgce_angle
+#$ -S /bin/sh
+#$ -cwd
+#$ -t 1:10
+#$ -tc 4
+
+. /etc/profile.d/modules.sh
+
+# Disable production of core dump files
+ulimit -c 0
+
+echo ""
+echo "***********************************************************************"
+echo " Starting job:"
+echo ""
+echo "    Name:              "$JOB_NAME
+echo "    ID:                "$JOB_ID
+echo "    Hostname:          "$HOSTNAME
+echo "    Working directory: "$SGE_O_WORKDIR
+echo ""
+echo "    Submitted using:   submit -N dimgce_angle --array 1:10 -i in/dimgce_mgmg_pes.in -o in/dimgce_mgmg_pes.out gen4.q psi4@master"
+echo "***********************************************************************"
+
+# cd into individual task directory
+cd $SGE_O_WORKDIR/$SGE_TASK_ID
+
+# Load the requested Psi4 module file
+vulcan load psi4@master~ambit~chemps2~debug~pcmsolver~vectorization
+
+export PSI_SCRATCH=$TMPDIR
+export KMP_DUPLICATE_LIB_OK=TRUE
+
+psi4 -n 4 -i in/dimgce_mgmg_pes.in -o in/dimgce_mgmg_pes.out
+
--- sub_pes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load psi4/master
-
-psi4 -i in/dimgce_pes/dimgce_pes-`$SGE_TASK_ID`.in -o out/dimgce_pes/dimgce_pes-`$SGE_TASK_ID`.out
