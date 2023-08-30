9af048e461c407977ebfd886a41ab0a2a78a6164
Connor Briggs <cgbriggs99@gmail.com>
2023-08-30 12:19:34 -0400

    Removed unnecessary files.

--- Dimagnesocene.sh
+++ Dimagnesocene.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$ -q gen4.q,gen6.q
-#$ -N dimgcp-
+#$ -N dimgcp
 #$ -S /bin/sh
 #$ -cwd
 
@@ -18,7 +18,7 @@ echo "    ID:                "$JOB_ID
 echo "    Hostname:          "$HOSTNAME
 echo "    Working directory: "$SGE_O_WORKDIR
 echo ""
-echo "    Submitted using:   submit -N dimgcp- -i in/dimagnesocene-.in -o out/dimagnesocene-.out gen4.q,gen6.q psi4@master"
+echo "    Submitted using:   submit -N dimgcp -i in/dimagnesocene.in -o out/dimagnesocene.out gen4.q,gen6.q psi4@master"
 echo "***********************************************************************"
 
 
@@ -28,5 +28,5 @@ vulcan load psi4@master~ambit~chemps2~debug~pcmsolver~vectorization
 export PSI_SCRATCH=$TMPDIR
 export KMP_DUPLICATE_LIB_OK=TRUE
 
-psi4 -n 4 -i in/dimagnesocene-.in -o out/dimagnesocene-.out
+psi4 -n 4 -i in/dimagnesocene.in -o out/dimagnesocene.out
 
