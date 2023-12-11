7503ba867d530d6a9fb2ed3ab7f76e2d8e745acc
Connor Briggs <cgbriggs99@gmail.com>
2023-12-11 15:11:36 -0500

    Finished a run

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#$ -q gen4.q
+#$ -N testing
+#$ -S /bin/sh
+#$ -cwd
+
+. /etc/profile.d/modules.sh
+
+# Disable production of core dump files
+ulimit -c 0
+
+echo ""
+echo "***********************************************************************"
+echo " Starting job:"
+echo ""
+echo "    Name:              "$JOB_NAME
+echo "    ID:                "$JOB_ID
+echo "    Hostname:          "$HOSTNAME
+echo "    Working directory: "$SGE_O_WORKDIR
+echo ""
+echo "***********************************************************************"
+
+echo $(lspci -vnn)
+
+
